5609896e3d44a32554f6ac156b722a29e04ccfbe
Tan Kuan Hoong <tkuanhoong@gmail.com>
2023-10-25 12:10:03 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/fruit_jus_168/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fruit_jus_168
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '168 Jus',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text(title)),
+    );
+  }
+}
