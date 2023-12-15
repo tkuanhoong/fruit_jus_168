94781fc62265d3e3735f7c533a8d499ae63c9089
Tan Kuan Hoong <tkuanhoong@gmail.com>
2023-12-16 03:27:27 +0800

    fix: menu loading ui

--- lib/features/menu/presentation/widgets/menu_loading.dart
+++ lib/features/menu/presentation/widgets/menu_loading.dart
@@ -5,33 +5,31 @@ class MenuLoadingIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.4,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          margin: const EdgeInsets.all(20),
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: const Color.fromARGB(39, 32, 148, 28),
           ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: const Color.fromARGB(39, 32, 148, 28),
-            ),
-            child: Column(
-              children: [
-                ConstrainedBox(
-                  constraints: const BoxConstraints(maxWidth: 250),
-                  child: const Text(
-                    'Welcome to 168 Jus! Please wait while we serve you the best juice in town!\n\n',
-                    textAlign: TextAlign.center,
-                  ),
+          child: Column(
+            children: [
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 250),
+                child: const Text(
+                  'Welcome to 168 Jus! Please wait while we serve you the best juice in town!\n\n',
+                  textAlign: TextAlign.center,
                 ),
-                const CircularProgressIndicator(),
-              ],
-            ),
-          )
-        ],
-      ),
+              ),
+              const CircularProgressIndicator(),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
