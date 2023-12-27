9ad75b9c941b51cc01fb3e1b454313fb081f9662
Tan Kuan Hoong <tkuanhoong@gmail.com>
2023-12-27 21:54:35 +0800

    new: payment integration

---
+++ lib/config/stripe/stripe_constants.dart
@@ -0,0 +1,2 @@
+// ignore: constant_identifier_names
+const STRIPE_PUBLIC_KEY = "pk_test_51NJXYOAARRMlaDk6RJoZYDMPzx1BwmAST8vw1HxJJhdn354ZvqWkqv0zbhg0ldy2a5g0tDIY09lqUZOCWsWErMpl00gQjiLls3";
--- lib/core/utility/initialize_app_settings.dart
+++ lib/core/utility/initialize_app_settings.dart
@@ -2,9 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:fruit_jus_168/config/stripe/stripe_constants.dart';
 import 'package:fruit_jus_168/core/utility/app_bloc_observer.dart';
 import 'package:fruit_jus_168/firebase_options.dart';
 import 'package:fruit_jus_168/core/utility/injection_container.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 Future<void> initializeAppSettings() async {
   // ensure that the flutter tree is initialized
@@ -20,6 +23,10 @@ Future<void> initializeAppSettings() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+  // stripe payment
+  Stripe.publishableKey = STRIPE_PUBLIC_KEY;
+  // load env
+  await dotenv.load(fileName: "assets/.env");
   // initialize the dependencies (services locator)
   await initializeDependencies();
 }
