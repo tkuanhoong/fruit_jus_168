843b0f2bf62655d77059710957ec6346f25e5a0e
Tan Kuan Hoong <tkuanhoong@gmail.com>
2024-01-03 10:12:30 +0800

    asset: pickup photo

--- lib/config/routes/app_router.dart
+++ lib/config/routes/app_router.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_jus_168/config/routes/app_router_constants.dart';
